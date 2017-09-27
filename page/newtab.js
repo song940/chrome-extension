91466b2759b088952e12d3df1532785930f21d6f
Lsong <song940@gmail.com>
2017-09-27 19:16:58 +0800

    use bing image api for now

--- page/newtab.js
+++ page/newtab.js
@@ -1,3 +1,13 @@
 chrome.topSites.get(function(topSites){
-  console.log('newtab', topSites);  
+  console.log('newtab', topSites);
 });
+
+fetch('http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US', {})
+.then(res => res.json())
+.then(res => {
+  const [ image ] = res.images;
+  const { url, copyright, copyrightlink } = image;
+  document.getElementById('main').style.backgroundImage = `url(https://bing.com${url})`;
+  document.getElementById('copyright').href = copyrightlink;
+  document.getElementById('copyright').innerText = copyright;
+})
